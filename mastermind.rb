3c035cf533c9daea756b58e5ae8185e4ba4df8b7
Kat Lynch <kat@madebymany.co.uk>
2019-05-11 13:38:59 +0100

    first working version

---
+++ mastermind.rb
@@ -0,0 +1,218 @@
+COLORS = [:red, :green, :blue, :yellow, :purple, :orange]
+
+class Mastermind
+  def initialize
+    @display = Display.new
+    @codemaker = Codemaker.new(@display)
+    @codebreaker = Codebreaker.new(@display)
+  end
+
+  def play
+    round = Round.new(@display, @codemaker, @codebreaker)
+    round.play
+    if @display.get_play_again?
+      play
+    end
+  end
+
+  private
+
+  def self.get_result(code, guess)
+    guess.each_with_index.map do |guess_color, index|
+      if code[index] == guess_color
+        :black
+      elsif self.award_white?(code, guess, index, guess_color)
+        :white
+      else
+        :blank
+      end
+    end
+  end
+
+  def self.award_white?(code, guess, index, guess_color)
+    if code.include?(guess_color) 
+      guesses_of_color_checked = guess[0..index].select { |c| c == guess_color }
+      color_count_in_code = code.select { |c| c == guess_color }
+      guesses_of_color_checked.length <= color_count_in_code.length
+    else
+      false
+    end
+  end
+end
+
+class Round
+  def initialize(display, codemaker, codebreaker)
+    @codemaker = codemaker
+    @codebreaker = codebreaker
+    @display = display
+    @guess_count = 0
+  end
+
+  def play
+    code_broken = false
+    code = @codemaker.get_code
+    @display.show_code(code)
+    until code_broken
+      guess = @codebreaker.get_guess
+      if guess == code
+        code_broken = true
+      else
+        @guess_count += 1
+        result = Mastermind.get_result(code, guess)
+        @display.show_pegs(guess)
+        @display.show_pegs(result)
+      end
+    end
+    @display.display_victory(@guess_count)
+  end
+end
+
+class Codemaker
+  def initialize(display)
+    @display = display
+  end
+
+  def get_code
+    @code = 4.times.collect{ COLORS.sample }
+  end
+end
+
+class Codebreaker
+  def initialize(display)
+    @display = display
+  end
+
+  def get_guess
+    @display.get_guess
+  end
+end
+
+class Display
+
+  ICONS = {
+    red: "🔴",
+    green: "💚",
+    blue: "💙",
+    yellow: "💛",
+    purple: "💜",
+    orange: "🍊",
+    blank: "❌",
+    black: "⚫️",
+    white: "⚪️",
+    hidden: "❓"
+  }
+
+
+  def initialize
+    @first_guess = true
+    @colorize = false
+    if Gem::Specification::find_all_by_name('colorize').any?
+      require 'colorize'
+      @colorize = true
+      @colors = {
+        red: :red,
+        green: :green,
+        blue: :blue,
+        yellow: :yellow,
+        purple: :magenta,
+        orange: [:red, :yellow],
+        black: :black,
+        white: :white
+      }
+    end
+  end
+
+  def get_name
+    get_input("What is your name?")
+  end
+
+  def get_guess
+    guess_input = get_input(get_guess_msg)
+    letters_lookup = COLORS.map{ |c| [c[0], c] }.to_h
+    letters = letters_lookup.keys
+    matches = guess_input.scan(/[#{letters.join}]/)
+    if matches.length != 4
+      output("please enter 4 colours #{colour_letters}")
+      get_guess
+    else
+      matches.map{ |l| letters_lookup[l] }
+    end
+  end
+
+  def get_guess_msg
+    if @first_guess
+      @first_guess = false
+      colour_letters
+    else
+      ""
+    end
+  end
+
+  def colour_letters
+    if @colorize
+      COLORS.map do |c|
+        if @colors[c].is_a?(Symbol)
+          c[0].colorize(color: @colors[c], background: :grey)
+        else
+          c[0].colorize(color: @colors[c][0], background: @colors[c][1])
+        end
+      end.join
+    else
+      COLORS.map { |c| c[0] }.join
+    end
+  end
+
+  def get_play_again?
+    input = get_input("Play again? [yn]")
+    ["y", "yes"].include?(input.downcase)
+  end
+
+  def get_input(message)
+    output("#{message} > ", :print)
+    gets.strip
+  end
+
+  def show_code(code)
+    output("Code has been chosen!")
+    show_pegs(code, false)
+    output("Guess from 6 colours", :print)
+    show_pegs(COLORS)
+  end
+
+  def show_pegs(pegs, hide = false)
+    if hide
+      output(4.times.collect{ ICONS[:hidden] }.join)
+    else
+      output pegs.map{ |p| ICONS[p] }.join
+    end
+  end
+
+  def display_victory(guess_count)
+    output "Hooray! You won after #{guess_count} guesses!"
+  end
+
+  def output(message, function = :puts)
+    if @colorize
+      send(function, message.colorize(:background => :light_black))
+    else
+      send(function, message)
+    end
+  end
+end
+
+m = Mastermind.new
+m.play
+# def color(index)
+#   normal = "\e[#{index}m#{index}\e[0m"
+#   bold = "\e[#{index}m\e[1m#{index}\e[0m"
+#   "#{normal}  #{bold}  "
+# end
+
+# 10.times do|index|
+#   line = color(index + 1)
+#   line += color(index + 30)
+#   line += color(index + 90)
+#   line += color(index + 40)
+#   line += color(index + 100)
+#   puts line
+# end
